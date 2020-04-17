b68db3ff756e5500a5e74a5082fb8a8750f92403
Dietrich Zinsou <60661924+dzinsouhpe@users.noreply.github.com>
2020-04-17 16:14:10 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mlflow server --backend-store-uri $MLFLOW_BACKEND_STORE --host 0.0.0.0
+mlflow server --backend-store-uri $MLFLOW_BACKEND_STORE --default-artifact-root $MLFLOW_S3_ENDPOINT_URL --host 0.0.0.0
